2d7d71973c161bbf217d70aba627e1b2ad881409
Chris O'Hara <cohara87@gmail.com>
2011-08-21 12:22:21 +1000

    Split out embedded routes, and don't auto-render views

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ require('./lib/request');
 
 module.exports = models;
 module.exports.createRoutes = rest.create;
+module.exports.id_format = rest.id_format;
+module.exports.createEmbeddedRoutes = rest.createEmbedded;
 module.exports.autoloadResources = rest.autoloadResources;
 module.exports.generateBackbone = backbone.generate;
 module.exports.backboneHelpers = backbone.helpers;
